cf6159b4282c163b6f8ee2ede3aa2e29708f9b9a
tschmidt6 <36741011+tschmidt6@users.noreply.github.com>
2018-04-03 17:17:08 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayList;
 
 /**
  * Undirected and unweighted graph implementation
@@ -15,13 +16,34 @@ public class Graph<E> implements GraphADT<E> {
     /**
      * Instance variables and constructors
      */
+    private ArrayList<ArrayList<Boolean>> edges; 	// 2D array for edges
+    private ArrayList<E> vertices; 				    // list of vertices
+    
+    public Graph() {
+        vertices = new ArrayList<E>();
+        edges = new ArrayList<ArrayList<Boolean>>();
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public E addVertex(E vertex) {
-        
+        if (vertex == null) {
+            return null;
+        }
+        int i = vertices.indexOf(vertex);
+        if (i == -1) {
+            vertices.add(vertex);                   // Append to vertices (will always be last index)
+            edges.add(new ArrayList<Boolean>());    // New Row
+            for (ArrayList<Boolean> startPoint : edges) {   // Edges in square matrix
+                while (startPoint.size() != vertices.size()) {
+                    startPoint.add(false);          // Appending Column
+                }
+            }
+            return vertex;
+        }
+        return null;
     }
 
     /**
